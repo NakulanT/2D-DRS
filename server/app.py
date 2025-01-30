86f105dff2d022149f41329d4cf92ab2ae17521c
NakulanT <nakult721@gmail.com>
2025-01-30 22:04:51 +0530

    Feat : Chnaged from falsk to fastapi

--- server/app.py
+++ server/app.py
@@ -1,13 +1,15 @@
-from flask import Flask
-app = Flask(__name__)
+from fastapi import FastAPI
 from LBWDetection import LBWDetectionModel
 
-@app.route("/")
+app = FastAPI()
+
+@app.get("/")
 def hello():
-    return "Hello World!"
+    return {"message": "Hello World!"}
 
 if __name__ == "__main__":
     model = LBWDetectionModel()
-    print(model.get_result(input_video_path = 'video21.mp4',stump_img_path = 'frame6.jpg'))
+    print(model.get_result(input_video_path='video21.mp4', stump_img_path='frame6.jpg'))
     
-    app.run()
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
